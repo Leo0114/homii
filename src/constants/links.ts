9615c3c2779da84dcf84761f098ba968265b6a89
Leo0114 <leonel0614@gmail.com>
2026-07-31 14:43:53 -0600

    update no google play

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,4 +1,8 @@
 export const APP_LINKS = {
-  APPLE_STORE: "https://apps.apple.com/mx/app/homii-cozy-living/id6758175368?l=en-GB",
-  GOOGLE_PLAY: "https://play.google.com/store/apps/details?id=net.homii.residents"
+  APPLE_STORE:
+    "https://apps.apple.com/mx/app/homii-cozy-living/id6758175368?l=en-GB",
+  GOOGLE_PLAY:
+    "https://play.google.com/store/apps/details?id=net.homii.residents",
 };
+
+// Google play not ready yet
